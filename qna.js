f2941b929b91bee89bd390349c9768dbb22b2923
KayKhaing-1899 <chawkaykhaing209@gmail.com>
2022-10-31 10:55:57 +0630

    update

--- qna.js
+++ qna.js
@@ -2,6 +2,7 @@ const questions = document.querySelectorAll('#ques-ans')
 
 questions.forEach(function(q) {
     const btn = q.querySelector('.qbtn')
+    questions[0].classList.toggle("show-text")
     btn.addEventListener("click",function() {
         questions.forEach(function(i) {
             if (q !== i) {
