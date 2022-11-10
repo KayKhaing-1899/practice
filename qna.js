bfee7d00bfd7194fc84a1b60cacea52d158d2ad6
KayKhaing-1899 <chawkaykhaing209@gmail.com>
2022-11-10 10:58:46 +0630

    update

--- qna.js
+++ qna.js
@@ -13,12 +13,20 @@ questions.forEach(function(q) {
     })
 })
 
+const str = document.querySelectorAll("#text")
+
 window.onscroll = function() {
     if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80){
         document.getElementById("navibar").style.background = "lightgray"
+        str.forEach(function(j) {
+            j.style.color = "black"
+        })
     }
     else{
         document.getElementById("navibar").style.background = "none"
+        str.forEach(function(j) {
+            j.style.color = "white"
+        })
     }
         
 }
